6a436520304877accb06c8c404326d97ed7907f6
botrosco <51170107+botrosco@users.noreply.github.com>
2020-08-12 19:24:30 +0000

    Update wg-peergen.sh

--- wg-peergen.sh
+++ wg-peergen.sh
@@ -1,8 +1,10 @@
 # Set vars
-ip="192.168.2." #Root IP Address. Only include first 3 segments, the final one is sorted in exec.
+ip="192.168.2" #Root IP Address. Only include first 3 segments, the final one is sorted in exec.
 ext_ip="" #URL/IP for the connection to wireguard server
 port="55555" #Port for the wireguard server
 dns="1.1.1.1,1.0.0.1" #Comma seperated list
+svrkeypub=$(sudo cat "/etc/wireguard/server.key.pub") #Location of server public key
+svrkeypvt=$(sudo cat "/etc/wireguard/server.key") #Location of server private key
 
 # Sort dir stuff
 dir=$(pwd);
@@ -10,7 +12,7 @@ cd /tmp;
 
 # Collect device configs
 read -p "Peer name: " p_name;
-read -p "Number of device (3 would be 192.168.2.3/32): " p_ip;
+read -p "Number of device (3 would be $ip.3/32): " p_ip;
 
 # Generate keys using colleceted info
 wg genkey | sudo tee $p_name.key | wg pubkey | sudo tee $p_name.key.pub;
@@ -20,13 +22,12 @@ keypvt=$(cat $p_name.key);
  #echo "Pvt key: $keypvt"; #For troubleshooting
 keypub=$(cat $p_name.key.pub);
  #echo "Pub key: $keypub"; #For troubleshooting
-keypubsvr=$(sudo cat /etc/wireguard/publickey);
  #echo "Pub key: $keypubsvr"; #For troubleshooting
 
 #Generate conf
 echo "[Interface]
 PrivateKey = $keypvt
-Address = $ip$p_ip/32
+Address = $ip.$p_ip/32
 DNS = $dns
 
 [Peer]
@@ -36,7 +37,7 @@ Endpoint = $ext_ip:$port" >> "$p_name.conf";
  #cat "$p_name.conf"; #For troubleshooting
 
 # Run "wg set" for the new device
-sudo wg set wg0 peer $keypub allowed-ips 192.168.2.$p_ip/32 &&
+sudo wg set wg0 peer $keypub allowed-ips $ip.$p_ip/32 &&
  #echo "sudo wg set wg0 peer $keypub allowed-ips $ip$p_ip/32"; #For troubleshooting
 
 # Generate QR
