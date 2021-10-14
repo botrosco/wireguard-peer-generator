053a103b64decc07d350d3917ccdd2d6b1fba803
botrosco <51170107+botrosco@users.noreply.github.com>
2021-10-14 11:32:55 +0100

    Add files via upload

--- wg-peergen.sh
+++ wg-peergen.sh
@@ -1,16 +1,16 @@
 # Set vars
-ip="192.168.2" #Root IP Address. Only include first 3 segments, the final one is sorted in exec.
+ip="10.1.1" #Root IP Address. Only include first 3 segments, the final one is sorted in exec.
 ext_ip="" #URL/IP for the connection to wireguard server
-port="55555" #Port for the wireguard server
-al_ip="10.0.0.0/24,192.168.1.0/24" #AllowdIP (Applied if using split tunnel)
-dns="1.1.1.1,1.0.0.1" #Comma seperated list
-svrkeypub=$(sudo cat "/etc/wireguard/server.key.pub") #Location of server public key, for the provided guide it would be /etc/wireguard/keys/server.key.pub
-svrkeypvt=$(sudo cat "/etc/wireguard/server.key") #Location of server private key, for the provided guide it would be /etc/wireguard/keys/server.key
+port="" #Port for the wireguard server
+al_ip="10.1.1.0/24,192.168.0.0/24" #AllowdIP (Applied if using split tunnel)
+dns="10.1.1.1" #Comma seperated list
+svrkeypub=$(cat "/etc/wireguard/server.key.pub") #Location of server public key
+svrkeypvt=$(cat "/etc/wireguard/server.key") #Location of server private key
 wg_clients_dir="$HOME/wg-clients/" #Location to place conf files for qr scanning at a later date
 
 # Sort dir stuff
 dir=$(pwd);
-cd /tmp;
+cd /wg-tmp;
 
 # Collect device configs
 read -p "Peer name: " p_name;
@@ -18,23 +18,23 @@ read -p "Number of device (3 would be $ip.3/32): " p_ip;
 read -p "Split tunnel? (y/n): " split;
 
 # Generate keys using colleceted info
-wg genkey | sudo tee $p_name.key | wg pubkey | sudo tee $p_name.key.pub;
+wg genkey | tee $p_name.key | wg pubkey > $p_name.key.pub;
 
 # Keys as vars
 keypvt=$(cat $p_name.key);
  #echo "Pvt key: $keypvt"; #For troubleshooting
 keypub=$(cat $p_name.key.pub);
  #echo "Pub key: $keypub"; #For troubleshooting
- #echo "Pub key: $keypubsvr"; #For troubleshooting
- 
+ #echo "Pub key: $svrkeypub"; #For troubleshooting
+
 # Sort tunnel type settings
 case $split in
 y)
- al_ip_fin="$al_ip"
- ;;
+	al_ip_fin="$al_ip"
+	;;
 n)
- al_ip_fin="0.0.0.0/0"
- ;;
+	al_ip_fin="0.0.0.0/0"
+	;;
 esac
 
 #Generate conf
@@ -45,20 +45,22 @@ DNS = $dns
 
 [Peer]
 PublicKey = $svrkeypub
-AllowedIPs = $al_ip_fin
+AllowedIPs = "$al_ip_fin"
 Endpoint = $ext_ip:$port" >> "$p_name.conf";
- #cat "$p_name.conf"; #For troubleshooting
-
-# Run "wg set" for the new device
-sudo wg set wg0 peer $keypub allowed-ips $ip.$p_ip/32 &&
- #echo "sudo wg set wg0 peer $keypub allowed-ips $ip$p_ip/32"; #For troubleshooting
 
 # Generate QR
-qrencode -t ansiutf8 < "$p_name.conf";
+ #echo ""
+ #qrencode -t ansiutf8 < "$p_name.conf";
+
+# Print data for wireguard on router
+echo ""
+echo "Information for the WireGuard peer interface:"
+echo "- Description: $p_name"
+echo "- Public Key: $keypub"
+echo "- Allowed IPs: $ip.$p_ip/32"
 
 # Move stuff to wireguard folder
-cp -f "$p_name.conf" "$wg_clients_dir"; #Comment out to only place in /etc/wireguard/clients
-sudo mv "$p_name.conf" "$p_name.key" "$p_name.key.pub" "/etc/wireguard/clients/"; 
+cp -f "$p_name.conf" "$p_name.key" "$p_name.key.pub" "$wg_clients_dir";
 
 # Return to initial dir
 cd "$dir"
